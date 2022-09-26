b7790c50defb9642c3ba1eee8138c3428646cfb1
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-09-26 21:26:33 +0200

    Add 'pages' so you can move across the memory grid
    Add fill_grid() for filling all cells and displaying page number
    Add raw(), noecho() and keypad()
    Add key reading for page navigation

--- src/interface.h
+++ src/interface.h
@@ -1,11 +1,11 @@
 
 #define REFRESH_0() move(0,0); refresh();
-#define CLR_LINE(y) move(y,0); clrtoeol();
+#define CLR_LINE(y) move(y,0); clrtoeol();      // TODO: Save old cursor pos and restore after clear
 
 #define GRID_X 5            // Horizontal margin for the cell grid
 #define GRID_Y 3
-#define GRID_C 30           // Max number of items. Items per page should change depending on the terminal size
-int GRID_CPP = GRID_C;      // Cells displayed per page. Will be calculated in draw_grid()
+#define GRID_C 200          // Max number of items. Items per page should change depending on the terminal size
+int GRID_CPP = 30;          // Cells displayed per page. Will be calculated depenging on terminal width in draw_grid()
 #define MIN_GRID_CW 1       // Min width of the cells 
 int GRID_CW = MIN_GRID_CW;  // Default width of cells. Will change if we try to display large numbers
 
@@ -22,7 +22,7 @@ void draw_grid() {
     GRID_CPP = cells;
 
     // TODO: DELME
-    mvprintw(10, 10, "W: %d | Cells: %d", term_w, cells);
+    mvprintw(x, 10, "[Debug] W: %d | Cells: %d", term_w, cells);
 
     // First clear the cell lines to remove residual chars from resizes, etc.
     CLR_LINE(y);
@@ -34,7 +34,6 @@ void draw_grid() {
     mvprintw(y+1, x, "|");
     mvprintw(y+2, x, "+");
 
-    // TODO: Add 'next page' options (n = cells * cur page number)
     for (int n = 0; n < cells; n++) {
         /* Center of each cell. We use +1 because of the initial left border and 2*n because
          * its the space of a cell times the current cell number:
@@ -79,6 +78,29 @@ void fill_cell(int idx, const char* str) {
 
     // Center of y, base x pos + leftmost border + (right border of the cell + cell width) * cell number
     mvprintw(gy+1, gx+1 + (1+gcw) * idx, "%s", str);
+
+    REFRESH_0();
+}
+
+
+// Calls fill_cell for the necesary items to fill the whole grid
+void fill_grid(int* cell_arr, int page) {
+    char* buff= calloc(255, sizeof(char));
+    int real_idx = 0;
+
+    for (int n = 0; n < GRID_C; n++) {
+        // cells per page * current page + pos indide page
+        real_idx = GRID_CPP * page + n;
+
+        // Save int at real array pos to buff as long as we are in bounds
+        sprintf(buff, "%d", (real_idx < GRID_C) ? cell_arr[real_idx] : 0);
+
+        // Print the buff str at the screen pos
+        fill_cell(n, buff);
+    }
+
+    // Print page numbers. Keep in mind that we add one so its '1/5' instead of '0/4'
+    mvprintw(GRID_Y + 3, GRID_X, "[Page %d/%d]", page+1, GRID_C/GRID_CPP+1);
 
     REFRESH_0();
 }
--- src/main.c
+++ src/main.c
@@ -7,20 +7,29 @@
 #include "interface.h"
 
 int main() {
-    initscr();      // Init ncurses
+    initscr();              // Init ncurses
+    raw();                  // Scan input without pressing enter
+    noecho();               // Don't print when typing
+    keypad(stdscr, TRUE);   // Enable keypad (arrow keys)
 
-    // Declare and initialize as -1 the cell array for the memory simulation
-    int* cell_arr = calloc(GRID_C, sizeof(int));
+    // Declare and initialize as 0 the cell array for the memory simulation
+    int* cell_arr = malloc(GRID_C * sizeof(int));
+    for (int n = 0; n < GRID_C; n++)
+        cell_arr[n] = n;
 
-    for (int n = 0; n < GRID_C; n++) {
-        cell_arr[n] = 100 + n;
-    }
     cell_arr[5] = 1337;
+    cell_arr[130] = 1337;
 
     mvprintw(1, GRID_X, "Press 'q' to exit...");
 
+    int cur_page  = 0;
+    int max_pages = 0;      // Max pages of the grid. Updated each loop because the terminal size might change
+
+    int c = 0;
     // Loop while we are not pressing 'q'
     do {
+        max_pages = GRID_C/GRID_CPP;
+
         // First set GRID_CW to the max str len
         for (int n = 0; n < GRID_C; n++) {
             int digits = get_digits(cell_arr[n]);
@@ -29,13 +38,27 @@ int main() {
         }
 
         draw_grid();
-        
-        char* buff= calloc(255, sizeof(char));
-        for (int n = 0; n < GRID_C; n++) {
-            sprintf(buff, "%d", cell_arr[n]);
-            fill_cell(n, buff);
+        fill_grid(cell_arr, cur_page);
+
+        // Get the pressed key after drawing everything
+        c = getch();
+        switch (c) {
+            case 261:       // Right arrow
+                if (cur_page < max_pages)
+                    cur_page++;
+                break;
+            case 260:       // Left arrow
+                if (cur_page > 0)
+                    cur_page--;
+                break;
+            default:
+                break;
         }
-    } while (tolower(getchar()) != 'q');
+
+        // DELME
+        mvprintw(11, GRID_X, "[Debug] Pressed key: %d", c);     // TODO: Update CLR_LINE and call
+        refresh();
+    } while (tolower(c) != 'q');
     
     endwin();       // End ncurses window
     return 0;
